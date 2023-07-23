a611e431842940e5ba5ec8f733d78b07b3d364e8
Diego Ploche <diegoploche86@gmail.com>
2023-07-23 15:21:09 -0500

    Added simple css and html. Also installed modules and auhtorized with salesforce successfully.

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const authorizeSF = require('./middleware/authorize-sf.js');
 
 app.use(express.static('./public'));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Home Page
+
 app.get('/', (req, res) => {
-    res.status(200).send('./public/index.html');
+    res.status(200).sendFile(path.resolve(__dirname, './public/index.html'));
+});
+
+// Lead Get and Post
+
+app.get('/lead', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, './public/lead.html'));
+});
+
+app.post('/lead', (req, res) => {
+    res.status(201).send('Created Lead');
+});
+// Case Get and Post
+
+app.get('/case', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, './public/case.html'));
+});
+
+// Authorization test
+
+app.post('/auth', authorizeSF, (req, res) => {
+    if (req.authorization && req.authorization.success) {
+        console.log('Access Token:', req.authorization.accessToken);
+        res.status(201).send('Successful Authorization');
+    } else {
+        console.log('Authorization Error:', req.authorization.error);
+        res.status(500).send(req.authorization.message);
+    }
 });
 
 app.listen(5500, (req, res) => {
     console.log('Listening on Port 5500...');
+    //console.log(consumerKey, consumerSecret);
 });
---
+++ middleware/authorize-sf.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const env = process.env;
+const axios = require('axios');
+const qs = require('qs');
+
+const config = {
+    grant_type : 'password',
+    client_id : env.consumerKey,
+    client_secret : env.consumerSecret,
+    username : env.username,
+    password : env.password + env.securityToken,
+};
+
+function authorizeSF(req, res, next){
+    const authData = config;
+    const data = qs.stringify(authData);
+
+    axios.post('https://login.salesforce.com/services/oauth2/token', data, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+    .then(response => {
+        req.authorization = {
+            success: true,
+            accessToken: response.data.access_token
+        };
+        next();
+    })
+    .catch(error => {
+        req.authorization = {
+            success: false,
+            message: 'An error occurred during authorization',
+            error: error.response ? error.response.data : error.message
+        };
+        next();
+    });
+}
+
+module.exports = authorizeSF;
